ecee421eb3eaab4bc147dbfbca5729034c1d7c5e
Agustin Correa <128837559+AgustinCNz@users.noreply.github.com>
2023-07-07 16:25:59 -0300

    Clase 6 Formulario de registro
    Agregue modificaciones sugeridas por el profesor crack Santiago Ocon
    modificaciones en el html:
    1)cambie la etiqueta nav a main
    2)agegue un video mp4 de fondo
    3)agregue una etiqueta <p> en cada texto del input para poder darle un hover al texto dentro de cada p
    4)agregue un loading infinito en los input de aceptacion de terminos de condiciones
    5)agregue un loading infinito en input de Subscribirse para obtener nueva información!
    6)en los input ingrese un hover para cuando se quiera ingresar texto incremente el width y tenga un desplazamiento 
    en css hice modificaciones las cuales detallo
    1) agregue en general estilos con * { box-sizing: border-box; margin: 0; padding: 0;} 
    2)cambie los colores nombrados #fff
    3) los input type text & password poseen un transition: widh 0.3 ease-in-out; 
    esto seria Agregamos una transición de 0.3 segundos a la propiedad width
    4) las etiquetas p tienen un cursor pointer
    5) main posee un z-index: 1; para que se posiciones sobre el video de fondo
    6) en el .section input agregamos position: relative; /* Aqui  posición relativa para poder posicionar el pseudo-elemento */
        z-index: 1; /* Nos Aseguramos que el campo de entrada esté por encima del pseudo-elemento */
    7)el section input::after nos ayuda a que es lo que pasa despues en el input

--- Clases-Agustin/Clase6/index.js
+++ Clases-Agustin/Clase6/index.js
@@ -1,6 +1,14 @@
 function validateForm(event) {
     event.preventDefault();
-  
+
+    /* Función para mostrar mensajes de error */
+    function showError(message) {
+        const errorElement = document.getElementById("error-message");
+        errorElement.textContent = message;
+        errorElement.style.display = "block";
+    }
+    
+
     const firstName = document.getElementById("first-name").value;
     const lastName = document.getElementById("last-name").value;
     const username = document.getElementById("username").value;
@@ -9,60 +17,57 @@ function validateForm(event) {
     const birthdate = document.getElementById("birthdate").value;
     const newsletterCheckbox = document.getElementById("newsletter-checkbox").checked;
     const termsCheckbox = document.getElementById("terms-checkbox").checked;
-    
+
     /* Validación de campos */
     if (!firstName || !lastName || !username || !password || !confirmPassword || !birthdate) {
         showError("Por favor verifique sus datos ingresados");
         return;
     }
-  
+
     /* Verificación de contraseña y confirmación */
     if (password !== confirmPassword) {
         showError("Las contraseñas no coinciden.");
         return;
     }
-  
+
+    const myRegExp = new RegExp(/^[A-Za-z\s]+$/)
     /* Validación de caracteres permitidos en campos de texto */
-    if (!/^[A-Za-z\s]+$/.test(firstName) || !/^[A-Za-z\s]+$/.test(lastName) || !/^[A-Za-z\s]+$/.test(username)) {
+    if (myRegExp.test(firstName) || !myRegExp.test(lastName) || !myRegExp.test(username)) {
         showError("Solo se permiten letras y espacios en los campos de texto.");
         return;
     }
-  
+    
+    const myRegExp2 = new RegExp(/\d/)
+    const myRegExp3 = new RegExp(/[a-zA-Z]/)
     /* Validación de contraseña con letras y números */
-    if (!/\d/.test(password) || !/[a-zA-Z]/.test(password)) {
+    if (!myRegExp2.test(password) || !myRegExp3.test(password)) {
         showError("La contraseña debe contener letras y números.");
         return;
     }
-  
+
     /* Validación de edad */
     const currentDate = new Date();
     const birthdateDate = new Date(birthdate);
     const age = currentDate.getFullYear() - birthdateDate.getFullYear();
     const birthdateDay = birthdateDate.getDate();
-  
+
     if (age < 18 || (age === 18 && currentDate < birthdateDate)) {
         showError("Debes ser mayor de edad.");
         return;
     }
-  
+
     /* Validación de checkbox de boletín y términos */
     if (!newsletterCheckbox || !termsCheckbox) {
         showError("Por favor acepte los términos y condiciones.");
         return;
     }
-  
-    /* Función para mostrar mensajes de error */
-    function showError(message) {
-        const errorElement = document.getElementById("error-message");
-        errorElement.textContent = message;
-        errorElement.style.display = "block";
-    }
-  
+
+    
+
     /* Inicializar el selector de fecha */
     flatpickr("#birthdate", {
         dateFormat: "d-m-Y"
     });
-  }
-  
-  document.getElementById("registration-form").addEventListener("submit", validateForm);
-  
+}
+
+document.getElementById("registration-form").addEventListener("submit", validateForm);
